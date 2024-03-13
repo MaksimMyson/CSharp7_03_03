216cb3b698edf5be7a6eb156401233d517ba2ae5
MaksimMyson <maksimmyson33@gmail.com>
2024-03-13 13:03:47 +0200

    exe-1

--- CSharp7_03_03/Program.cs
+++ CSharp7_03_03/Program.cs
@@ -1,10 +1,57 @@
-﻿namespace CSharp7_03_03
+﻿using System.Reflection.Metadata;
+
+namespace CSharp7_03_03
 {
-    internal class Program
+
+    public interface IOutput
+    {
+        void Show();
+        void Show(string info);
+    }
+
+    public class Array : IOutput
+    {
+        private int[] elements;
+
+        public Array(int[] arr)
+        {
+            elements = arr;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Елементи масиву:");
+            foreach (var element in elements)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void Show(string info)
+        {
+            Console.WriteLine("Елементи масиву:");
+            foreach (var element in elements)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine("\nІнформація: " + info);
+        }
+    }
+
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            int[] arr = { 1, 2, 3, 4, 5 };
+            Array array = new Array(arr);
+
+            array.Show();
+
+
+            array.Show("Це інформаційне повідомлення про масив.");
         }
     }
 }
